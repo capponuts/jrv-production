419e3e6cb41395674928e32f8a367e082b82221b
JRV Production <jrv.production85@gmail.com>
2025-11-20 22:26:47 +0100

    fix(build): recréer le composant SmoothScroll manquant

---
+++ src/components/SmoothScroll.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+
+export default function SmoothScroll() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    })
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    const id = requestAnimationFrame(raf)
+
+    return () => {
+      cancelAnimationFrame(id)
+      type LenisInstance = { destroy?: () => void }
+      const maybeLenis = lenis as unknown as LenisInstance
+      if (typeof maybeLenis.destroy === 'function') maybeLenis.destroy()
+    }
+  }, [])
+
+  return null
+}
+
